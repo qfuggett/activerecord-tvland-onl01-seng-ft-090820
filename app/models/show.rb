0ef79e843da19b88c74ed0403382e44c02b6f56f
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-12 18:13:20 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  belongs_to :characters
   
+  def actors_list
+    self.actors.map do |actor|
+      actor.name
+      
+    end
+  end
 end
